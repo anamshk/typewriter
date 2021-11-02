f3ca2bbd101565c02d04a7760cd7176dbdb2a3fc
Anam Shaikh <anam.shk@outlook.com>
2021-11-02 13:37:54 -0400

    commit with delay in line sentence

---
+++ typewriter.js
@@ -0,0 +1,49 @@
+
+setTimeout(() => {
+  process.stdout.write('h');
+}, 0);
+
+setTimeout(() => {
+  process.stdout.write('e');
+}, 50);
+
+setTimeout(() => {
+  process.stdout.write('l');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('l');
+}, 150);
+
+setTimeout(() => {
+  process.stdout.write('o');
+}, 200);
+
+setTimeout(() => {
+  process.stdout.write(' ');
+}, 250);
+
+setTimeout(() => {
+  process.stdout.write('t');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('h');
+}, 350);
+
+setTimeout(() => {
+  process.stdout.write('e');
+}, 400);
+
+setTimeout(() => {
+  process.stdout.write('r');
+}, 450);
+
+setTimeout(() => {
+  process.stdout.write('e');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 550);
+
